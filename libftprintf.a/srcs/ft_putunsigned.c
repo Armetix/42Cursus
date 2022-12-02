0e0cdd5c502fa98644bde06cbf25ecb87fb06984
Kevin Derhet <kderhet@fu-r5-p1.s19.be>
2022-12-02 13:03:59 +0100

    to do : Makefile, verif librairies, francinette

--- libftprintf.a/srcs/ft_putunsigned.c
+++ libftprintf.a/srcs/ft_putunsigned.c
@@ -6,53 +6,60 @@
 /*   By: kderhet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 12:04:25 by kderhet           #+#    #+#             */
-/*   Updated: 2022/12/02 13:02:55 by kderhet          ###   ########.fr       */
+/*   Updated: 2022/12/02 12:59:04 by kderhet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include "libft.h"
+#include <stdlib.h>
 
-int	ft_ptr_len(unsigned int n)
+int	ft_unsigned_len(unsigned int n)
 {
 	int	len;
 
 	while (n != 0)
 	{
 		len++;
-		n = n / 16;
+		n = n / 10;
 	}
 	return (len);
 }
 
-void	ft_printptr(unsigned int n, const char *format)
+void	ft_uitoa(unsigned int n)
 {
-	if (n >= 16)
+	char	*nbr;
+	int		len;
+
+	len = ft_unsigned_len(n);
+	nbr = (char *)malloc(sizeof(char) * (len + 1));
+	if (!nbr)
+		return (0);
+	nbr[len--] = '\0';
+	while (n != 0)
 	{
-		ft_puthexa(n / 16, format);
-		ft_puthexa(n % 16, format);
+		nbr[len] = '0' + (n % 10);
+		nb = nb / 10;
+		len--;
 	}
-	else
-	{
-		if (n <= 9)
-			ft_putchar_fd((n + '0'), 1);
-		else
-			ft_putchar_fd((n - 10 + 'a'), 1);
-	}
+	return (nbr);
 }
 
-int	ft_putptr(unsigned long long ptr)
+int	ft_putunsigned(unsigned int n)
 {
-	int len;
+	int		len;
+	char	*nbr;
 
 	len = 0;
-	len += write(1, "0x", 2);
-	if (ptr == 0)
+	if (n == 0)
 	{
 		write(1, '0', 1);
-		return (1);
+		len = 1;
 	}
 	else
-		ft_printptr(ptr);
-	return (ft_ptr_len(ptr));
+	{
+		nbr = ft_uitoa(n);
+		len += ft_putstr(nbr);
+		free(nbr);
+	}
+	return (len);
 }
