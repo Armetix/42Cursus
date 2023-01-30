e9bd50b418bed3413365baf091ca3e4c428aabe0
Kevin Derhet <kderhet@fu-r4-p1.s19.be>
2023-01-30 14:43:16 +0100

    get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,35 +1,53 @@
 #include "get_next_line.h"
+#include <stdio.h>
 
 char *get_next_line(int fd)
 {
     char static *stash;
     char *line;
+    char *buffer;
     int i;
     int j;
     int k;
+    int l;
     int check;
 
+    i = 0;
     j = 0;
-    stash = malloc(sizeof(char) * 250);
+    check = 1;
+    buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+        if (!buffer)
+            return (NULL);
     line = malloc(sizeof(char) * 250);
+    stash = malloc(sizeof(char) * 250);
     if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
         return (NULL);
-    check = read(fd, stash, BUFFER_SIZE);
-    while (check != 0)
+    while (check > 0)
     {
+        check = read(fd, buffer, BUFFER_SIZE);
         j += check;
-        i = 0;
-        //k = 0;
-        
-        while (i >= j)
+        k = 0;   
+        while (i < j)
         {
-            line[i] = stash[k];
+            stash[i] = buffer[k];
+            if (stash[i] == '\n')
+            {
+                l = 0;
+                while (l < i)
+                {
+                    line[l] = stash[l];
+                    l++;
+                }
+                i++;
+                return (line);
+            }
             i++;
             k++;
         }
-        check = read(fd, stash, BUFFER_SIZE);
+        
     }
-
+    if (check == 0)
+        return (NULL);
     return (line);
 }
 
@@ -39,6 +57,7 @@ char *get_next_line(int fd)
 int main(int argc, char const *argv[])
 {
     int fd1 = open("TestLine.txt", O_RDONLY, 0);
+    printf("%s\n", get_next_line(fd1));
     printf("%s\n", get_next_line(fd1));
     return 0;
 }
